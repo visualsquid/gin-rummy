4f4eb6bb0470881943a680a4925be10af118dc57
visualsquid <ben_yelverton@hotmail.com>
2019-02-02 19:22:36 +0000

    Added initial card-related classes; also added test project.

---
+++ gin-rummy/Cards/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gin_rummy.Cards
+{
+    class Card
+    {
+        public enum Rank { Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King }
+
+        public Suit Suit { get; set; }
+        public Rank RankValue { get; set; }
+
+        public Card(Suit suit, Rank rank)
+        {
+            this.Suit = suit;
+            this.RankValue = rank;
+        }
+
+    }
+}
---
+++ test-gin-rummy/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("test-gin-rummy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("test-gin-rummy")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7d3acdf9-9d18-4bbf-8ba9-d1922b1e5f49")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
