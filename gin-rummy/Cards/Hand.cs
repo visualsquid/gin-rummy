cb11084aa355eb325e86e6251a4326ccc8d0712d
visualsquid <ben_yelverton@hotmail.com>
2019-02-03 21:24:42 +0000

    Added initial implementation of Hand class, no tests yet.

---
+++ gin-rummy/Cards/Hand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gin_rummy.Cards
+{
+    class Hand
+    {
+        private List<Card> _cards;
+
+        public int Size { get { return _cards.Count; } }
+
+        public Hand()
+        {
+            _cards = new List<Card>();
+        }
+
+        public List<Card> ViewHand()
+        {
+            var cards = new List<Card>();
+            cards.AddRange(_cards);
+
+            return cards;
+        }
+
+        public bool RemoveCard(Card card)
+        {
+            return _cards.Remove(card);
+        }
+
+        public void AddCard(Card card)
+        {
+            _cards.Add(card);
+        }
+
+        public void AddAtPosition(Card card, int i)
+        {
+            _cards.Insert(i, card);
+        }
+
+    }
+}
