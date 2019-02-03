0fbffbf561a45a857890bb394006a242174857ba
visualsquid <ben_yelverton@hotmail.com>
2019-02-03 18:00:14 +0000

    Starting on Deck class...

--- gin-rummy/Cards/Deck.cs
+++ gin-rummy/Cards/Deck.cs
@@ -8,5 +8,14 @@ namespace gin_rummy.Cards
 {
     class Deck
     {
+        private List<Card> _cards;
+
+        public Deck()
+        {
+            _cards = new List<Card>();
+            InitialiseStandardDeck(_cards);
+        }
+
+        
     }
 }
